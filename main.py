037a3ff0a90cf7c4912ac54ec3bb8897e1adeb3e
root <root@vps.server.local>
2025-01-05 08:23:55 +0000

    add model 37000

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ai_training.training import train_agents
 
 train_agents(
     num_episodes=1000000,
-    start_episode=1800,
-    agent1_model_path="h5/1800_agent1.h5",
-    agent2_model_path="h5/1800_agent2.h5",
+    start_episode=3300,
+    agent1_model_path="h5/3300_agent1.h5",
+    agent2_model_path="h5/3300_agent2.h5",
 )
